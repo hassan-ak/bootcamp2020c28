ec480e3b0481ab061e1c09140f977ed1d76e2680
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-03 08:21:45 +0500

    P09-a-Create Components
    1:- Header component
    	a:- Created
    		i:- src\components\Header.tsx to define header component
    		ii:- logo image in asserts folder
    	b:- Updated
    		i:- src\App.tsx to add header component in the app
    		ii:- src\App.css to define styling for header component and the genral app
    2:- Home component
    	a:- Created
    		i:- src\components\Home.tsx to define Home component
    		ii:- Home image in asserts folder
    	b:- Updated
    		i:- src\App.tsx to add Home component in the app
    		ii:- src\App.css to define styling for Home component
    3:- Footer Component
    	a:- Created
    		i:- src\components\Footer.tsx to define Footer component
    	b:- Updated
    		i:- src\App.tsx to add Footer component in the app
    4:- About Component
    	a:- Created
    		i:- src\components\About.tsx to define About component
    	b:- Updated
    		i:- src\App.tsx to add About.tsx component in the app
    		ii:- src\App.css to add styling for about component
    5:- Store Component
    	a:- Created
    		i:- src\components\Store.tsx to define Store component
    	b:- Updated
    		i:- src\App.tsx to add Store.tsx component in the app
    		ii:- src\App.css to add styling for Store component
    6:- ErrorPage Component
    	a:- Created
    		i:- src\components\ErrorPage .tsx to define ErrorPage component
    	b:- Updated
    		i:- src\App.tsx to add ErrorPage component in the app
    		ii:- src\App.css to add styling for ErrorPage component

--- bucket/src/App.tsx
+++ bucket/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import About from './components/About';
+import { ErrorPage } from './components/ErrorPage';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Home } from './components/Home';
+import { Store } from './components/Store'
 
 function App() {
   return (
     <div>
-      Shoe Store App
+      <Header/>
+      <div className="">
+        <Home/>
+        <About/>
+        <Store/>
+        <ErrorPage/>
+      </div>
+      <Footer/>
     </div>
   );
 }
