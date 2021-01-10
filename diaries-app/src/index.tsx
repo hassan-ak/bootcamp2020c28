adb141a6cad8bd7a08e995f2a9b4434b20b5eed2
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-10 16:00:38 +0500

    Create Store

--- diaries-app/src/index.tsx
+++ diaries-app/src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './index.css';
+import App from './app/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { setupServer } from './services/mirage/server';
+import { Provider } from 'react-redux';
+import store from './store';
 
 if (process.env.NODE_ENV === 'development') {
   setupServer();
@@ -11,7 +14,9 @@ if (process.env.NODE_ENV === 'development') {
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+    <Provider store={store}>
       <App />
+    </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
