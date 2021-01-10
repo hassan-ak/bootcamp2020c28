61005aef4ee46dc774711a293d4f8f5808dc40d7
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-10 18:38:52 +0500

    Complete App

--- diaries-app/src/app/App.tsx
+++ diaries-app/src/app/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { FC, lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../rootReducer';
 
-function App() {
-  return (
-    <div>
-      Diaries app
-    </div>
+const Auth = lazy(() => import('../features/auth/Auth'));
+const Home = lazy(() => import('../features/home/Home'));
+
+const App: FC = () => {
+  const isLoggedIn = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
   );
-}
+  return (
+    <Router>
+        <Route path="/">
+          <Suspense fallback={<p>Loading...</p>}>
+            {isLoggedIn ? <Home /> : <Auth />}
+          </Suspense>
+        </Route>
+    </Router>
+  );
+};
 
 export default App;
