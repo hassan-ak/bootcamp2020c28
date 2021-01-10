e832ac5a03fe13dae78f0b2464c4dfe006809cbc
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-10 15:38:38 +0500

    Setup Mirrage Server

--- diaries-app/src/index.tsx
+++ diaries-app/src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { setupServer } from './services/mirage/server';
+
+if (process.env.NODE_ENV === 'development') {
+  setupServer();
+}
 
 ReactDOM.render(
   <React.StrictMode>
